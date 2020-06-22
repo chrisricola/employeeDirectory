d12cda8d9876315f202096ac9c722a573a807086
Chris Rico <chris.r.rico@gmail.com>
2020-06-21 23:33:02 -0700

    updated app and api js files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
+import SearchResultContainer from "./components/SearchResultContainer";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Header />
-      <Section />
-    </div>
-  );
+  return <SearchResultContainer />;
 }
 
 export default App;
