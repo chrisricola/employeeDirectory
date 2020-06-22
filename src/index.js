d21bf384d5dbbbdd539ca15a4c22495c9b3ab645
Chris Rico <chris.r.rico@gmail.com>
2020-06-21 22:57:50 -0700

    creating employee card and search form

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// create an API to get users...https://randomuser.me/api/?results=300&nat=us
+// create employee card that gets rendered to page
+// style the page
+// input funtion for search
+// search function to employee, button, filter
+// table of employees
